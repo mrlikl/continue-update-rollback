0baaf744e33a528db3edcfac162f8256f0853bbe
S Murali Krishnan <110023377+mrlikl@users.noreply.github.com>
2024-05-15 17:29:03 +0530

    refactor app

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 import boto3
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s',
-                    '--stack_arn', required=True,
-                    metavar='<Full-Stack-arn>',
-                    help='Specify the full stack arn of stack which is in UPDATE_ROLLBACK_FAILED')
-
-cfn = boto3.client('cloudformation')
-
-args = parser.parse_args()
 
 failed_nested_stacks = []
 failed_resources = []
@@ -19,11 +10,8 @@ update_failed_nested_stacks = []
 update_failed_nested_stacks_name = []
 nested_stack_arn_map = {}
 cancelled_status = 'Resource update cancelled'
-nested_skippable_statuses = ["DELETE_COMPLETE",
-                             "DELETE_IN_PROGRESS", "DELETE_FAILED"]
-start_event = "UPDATE_ROLLBACK_FAILED"
-end_event = "UPDATE_ROLLBACK_IN_PROGRESS"
 
+cfn = boto3.client('cloudformation')
 
 def get_stack_events(stack_arn):
     response = cfn.describe_stack_events(
@@ -35,6 +23,8 @@ def get_stack_events(stack_arn):
 def parse_stack_events(stack_events, is_root_stack):
     global skippable_resources
     global update_failed_nested_stacks
+    start_event = "UPDATE_ROLLBACK_FAILED"
+    end_event = "UPDATE_ROLLBACK_IN_PROGRESS"
     events_in = []
     if stack_events[0]['ResourceStatus'] == start_event and stack_events[1]['ResourceStatus'] != end_event:
         running = True
@@ -102,7 +92,6 @@ def describe_stack_status(stack_arn):
         print(response['ResponseMetadata'])
         return -1
 
-
 def describe_stack_resources(stack_arn):
     response = cfn.describe_stack_resources(
         StackName=stack_arn,
@@ -113,13 +102,7 @@ def describe_stack_resources(stack_arn):
         print(response['ResponseMetadata'])
         return -1
 
-
-root_stack = args.stack_arn
-
-root_stack_summary = describe_stack_status(root_stack)
-
-
-def pre_checks():
+def pre_checks(root_stack_summary):
     if "ParentId" in root_stack_summary or "RootId" in root_stack_summary:
         print("Pass the root stack arn")
         return False
@@ -128,61 +111,80 @@ def pre_checks():
         return False
     return True
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', required=True,
+                        metavar='[Full Stack ARN]',
+                        help='Specify the full stack ARN of stack which is in UPDATE_ROLLBACK_FAILED')    
+    args = parser.parse_args()
+    root_stack = args.stack_arn
+    root_stack_summary = describe_stack_status(root_stack)
+    global failed_nested_stacks
+    global failed_resources
+    global skippable_resources
+    global skippable_nested_stacks
+    global update_failed_nested_stacks
+    global update_failed_nested_stacks_name
+    global nested_stack_arn_map
+    nested_skippable_statuses = ["DELETE_COMPLETE",
+                             "DELETE_IN_PROGRESS", "DELETE_FAILED"]
+    if pre_checks(root_stack_summary):
+        resources = describe_stack_resources(root_stack)
+        failed_resources += find_failed_resources(resources)
 
-if pre_checks():
-    resources = describe_stack_resources(root_stack)
-    failed_resources += find_failed_resources(resources)
+        for i in failed_resources:
+            if i['ResourceType'] == 'AWS::CloudFormation::Stack':
+                failed_nested_stacks.append(i)
 
-    for i in failed_resources:
-        if i['ResourceType'] == 'AWS::CloudFormation::Stack':
-            failed_nested_stacks.append(i)
+        failed_resources = [
+            x for x in failed_resources if x not in failed_nested_stacks]
 
-    failed_resources = [
-        x for x in failed_resources if x not in failed_nested_stacks]
+        failed_resources = [
+            x['LogicalResourceId'] for x in failed_resources]
 
-    failed_resources = [
-        x['LogicalResourceId'] for x in failed_resources]
+        root_events = get_stack_events(root_stack)
 
-    root_events = get_stack_events(root_stack)
+        parse_stack_events(root_events, is_root_stack=True)
 
-    parse_stack_events(root_events, is_root_stack=True)
-
-    while failed_nested_stacks:
-        popped_stack = failed_nested_stacks.pop(0)
-        nested_stack_summary = describe_stack_status(
-            popped_stack['PhysicalResourceId'])
-        if nested_stack_summary['StackStatus'] in nested_skippable_statuses:
-            if popped_stack['StackId'] == root_stack:
-                nested_stack_arn_map[popped_stack['PhysicalResourceId']
-                                     ] = popped_stack['LogicalResourceId']
+        while failed_nested_stacks:
+            popped_stack = failed_nested_stacks.pop(0)
+            nested_stack_summary = describe_stack_status(
+                popped_stack['PhysicalResourceId'])
+            if nested_stack_summary['StackStatus'] in nested_skippable_statuses:
+                if popped_stack['StackId'] == root_stack:
+                    nested_stack_arn_map[popped_stack['PhysicalResourceId']
+                                        ] = popped_stack['LogicalResourceId']
+                else:
+                    nested_stack_arn_map[popped_stack['PhysicalResourceId']
+                                        ] = popped_stack['StackName'] + "."+popped_stack['LogicalResourceId']
+                skippable_nested_stacks.append(popped_stack['PhysicalResourceId'])
             else:
-                nested_stack_arn_map[popped_stack['PhysicalResourceId']
-                                     ] = popped_stack['StackName'] + "."+popped_stack['LogicalResourceId']
-            skippable_nested_stacks.append(popped_stack['PhysicalResourceId'])
+                nested_events = get_stack_events(
+                    popped_stack['PhysicalResourceId'])
+                parse_stack_events(nested_events, is_root_stack=False)
+                failed_resources += find_failed_resources_in_nested_stacks(
+                    popped_stack['PhysicalResourceId'])
+
+        failed_resources = [
+            value for value in failed_resources if value in skippable_resources]
+
+        update_failed_nested_stacks_name.extend(
+            nested_stack_arn_map[key] for key in update_failed_nested_stacks if key in nested_stack_arn_map
+        )
+
+        failed_resources.extend(x for x in update_failed_nested_stacks_name)
+
+        if not failed_resources:
+            print("No update failed resources")
+            cli_command = "aws cloudformation continue-update-rollback --stack-name " + \
+                root_stack_summary['StackName']
+            print(cli_command)
         else:
-            nested_events = get_stack_events(
-                popped_stack['PhysicalResourceId'])
-            parse_stack_events(nested_events, is_root_stack=False)
-            failed_resources += find_failed_resources_in_nested_stacks(
-                popped_stack['PhysicalResourceId'])
+            cli_command = "aws cloudformation continue-update-rollback --stack-name " + \
+                root_stack_summary['StackName']
+            cli_command += " --resources-to-skip "
+            cli_command += ' '.join(failed_resources)
+            print(cli_command)
 
-    failed_resources = [
-        value for value in failed_resources if value in skippable_resources]
-
-    update_failed_nested_stacks_name.extend(
-        nested_stack_arn_map[key] for key in update_failed_nested_stacks if key in nested_stack_arn_map
-    )
-
-    failed_resources.extend(x for x in update_failed_nested_stacks_name)
-
-    if not failed_resources:
-        print("No update failed resources")
-        cli_command = "aws cloudformation continue-update-rollback --stack-name " + \
-            root_stack_summary['StackName']
-        print(cli_command)
-    else:
-        cli_command = "aws cloudformation continue-update-rollback --stack-name " + \
-            root_stack_summary['StackName']
-        cli_command += " --resources-to-skip "
-        cli_command += ' '.join(failed_resources)
-        print(cli_command)
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='continue_update_rollback',
+    version='0.1',
+    description='A tool to generate CLI Command for continue update rollback of CloudFormation stacks',
+    author='Murali Krishnan',
+    python_requires='>=3.9',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cfn-cur = app:main'
+        ]
+    }
+)
